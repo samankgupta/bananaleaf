ea7ab7d61fe145a2a6f94dcec47f8d273b540a6b
Samank Gupta <samankgupta@gmail.com>
2020-09-09 01:06:47 +0530

    frontend

--- js.js
+++
@@ -1,7 +0,0 @@
-AOS.init();
-
-var loading=document.getElementById('loading');
-function load()
-{
-  setTimeout(function(){loading.style.display="none"}, 1000);
-}
---
+++ js/menujs.js
@@ -0,0 +1,41 @@
+document.getElementById("veg").addEventListener("click", function() {
+  var l1=document.getElementsByClassName("veg").length;
+  for (var i = 0; i < l1; i++) {
+  	document.getElementsByClassName("veg")[i].style.display="block";
+  }
+  var l2=document.getElementsByClassName("nonveg").length;
+  for (var i = 0; i < l2; i++) {
+  	document.getElementsByClassName("nonveg")[i].style.display="none";
+  }
+});
+document.getElementById("nveg").addEventListener("click", function() {
+  var l1=document.getElementsByClassName("nonveg").length;
+  for (var i = 0; i < l1; i++) {
+  	document.getElementsByClassName("nonveg")[i].style.display="block";
+  }
+  var l2=document.getElementsByClassName("veg").length;
+  for (var i = 0; i < l2; i++) {
+  	document.getElementsByClassName("veg")[i].style.display="none";
+  }
+});
+document.getElementById("both").addEventListener("click", function() {
+  var l1=document.getElementsByClassName("veg").length;
+  for (var i = 0; i < l1; i++) {
+  	document.getElementsByClassName("veg")[i].style.display="block";
+  }
+  var l2=document.getElementsByClassName("nonveg").length;
+  for (var i = 0; i < l2; i++) {
+  	document.getElementsByClassName("nonveg")[i].style.display="block";
+  }
+});
+
+window.addEventListener("scroll", function() {
+  var value=window.scrollY;
+  if(window.pageYOffset >350)
+  {
+  	document.getElementById("backtotop").style.display="block";	
+  }
+  else{
+  	document.getElementById("backtotop").style.display="none";		
+  }
+});
