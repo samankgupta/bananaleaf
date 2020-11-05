c3b81f140ce8c16c30d86a4763186b5aa939237f
samankgupta <58078902+samankgupta@users.noreply.github.com>
2020-11-05 18:12:16 +0530

    Update db.php

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,5 +1,5 @@
 <?php   
 
-$con = mysqli_connect("sql12.freemysqlhosting.net:3306","root","","sql12374635");
+$con = mysqli_connect("sql12.freemysqlhosting.net:3306","sql12374635","6HF4uzJSqn","sql12374635");
 
 ?> 
